cc218801e15f8a7d11dab36839785f5b28d96da5
Taiyob <oli1412001@gmail.com>
2024-09-03 16:26:14 +0600

    my new commit message

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,12 @@ import globalErrorHandler from './app/middleware/globalErrorHandler';
 const app: Application = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    credentials: true,
+  }),
+);
 app.use(cookieParser());
 
 // Application route
