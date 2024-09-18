e53f3b4e5705cc25edeacd18fd001427efee1200
Taiyob <oli1412001@gmail.com>
2024-09-18 16:31:10 +0600

    commit

--- src/app/builder/QueryBuilder.ts
+++ src/app/builder/QueryBuilder.ts
@@ -10,20 +10,20 @@ class QueryBuilder<T> {
     this.query = query || {};
   }
 
-  search(searchableFields: string[]) {
-    const carId = this?.query?.carId;
-    if (carId) {
-      this.modelQuery = this.modelQuery.find({
-        $or: searchableFields.map(
-          (field) =>
-            ({
-              [field]: { $regex: carId, $options: 'i' },
-            }) as FilterQuery<T>,
-        ),
-      });
-    }
-    return this;
-  }
+  // search(searchableFields: string[]) {
+  //   const carId = this?.query?.carId;
+  //   if (carId) {
+  //     this.modelQuery = this.modelQuery.find({
+  //       $or: searchableFields.map(
+  //         (field) =>
+  //           ({
+  //             [field]: { $regex: carId, $options: 'i' },
+  //           }) as FilterQuery<T>,
+  //       ),
+  //     });
+  //   }
+  //   return this;
+  // }
 
   /*
   search(searchableFields: string[]) {
@@ -39,6 +39,21 @@ class QueryBuilder<T> {
     return this;
   }
   */
+
+  search(searchableFields: string[]) {
+    const searchTerm = this?.query?.searchTerm;
+    if (searchTerm) {
+      this.modelQuery = this.modelQuery.find({
+        $or: searchableFields.map(
+          (field) =>
+            ({
+              [field]: { $regex: searchTerm, $options: 'i' },
+            }) as FilterQuery<T>,
+        ),
+      });
+    }
+    return this;
+  }
 
   filter() {
     const queryObj = { ...this.query };
